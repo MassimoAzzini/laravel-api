1bfce896b5942497a0789b9c056db245f7999b87
MassimoAzzini <massimo.azzini.web@gmail.com>
2023-12-11 15:31:54 +0100

    [add] image with asset

--- app/Http/Controllers/Api/PageController.php
+++ app/Http/Controllers/Api/PageController.php
@@ -10,12 +10,27 @@ class PageController extends Controller
 {
     public function index(){
         $projects = Project::with('technologies', 'type')->paginate(8);
+        foreach ($projects as $project) {
+            if($project->image){
+                $project->image = asset('storage/' . $project->image);
+            }else{
+                $project->image = asset('storage/uploads/placeholder.jpg');
+            };
+        }
 
         return response()->json($projects);
     }
 
+
     public function getProject($slug){
         $project = Project::where('slug', $slug)->with('technologies', 'type')->first();
+
+        if($project->image){
+            $project->image = asset('storage/' . $project->image);
+        }else{
+            $project->image = asset('storage/uploads/placeholder.jpg');
+        };
+
         if($project) $success = true;
         else $success = false;
 
